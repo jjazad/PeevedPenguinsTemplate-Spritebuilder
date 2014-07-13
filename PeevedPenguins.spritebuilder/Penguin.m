45abcca784e51b1122d9609aef16f97cbc00fb75
John <jzofrea@yahoo.com>
2014-07-13 09:44:49 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Penguin.m
@@ -0,0 +1,25 @@
+//
+//  Penguin.m
+//  PeevedPenguins
+//
+//  Created by John Zofrea on 7/13/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Penguin.h"
+
+@implementation Penguin
+
+-(id) init {
+
+    self = [super init];
+
+    if (self) {
+        CCLOG(@"Penguin Created");
+    }
+
+    return self; 
+}
+
+
+@end
