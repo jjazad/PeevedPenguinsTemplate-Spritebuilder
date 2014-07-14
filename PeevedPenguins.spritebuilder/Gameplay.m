b260616d2faf503b6d245df50c515df7b6c9eb08
John <jzofrea@yahoo.com>
2014-07-14 14:45:06 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -41,7 +41,6 @@ _physicsNode.debugDraw = TRUE;
     // called on every touch in this scene
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
 
-    NSLog(@"I am touching it"); 
          CGPoint touchLocation = [touch locationInNode:_contentNode];
         // start catapult dragging when a touch inside of the catapult arm occurs
     if (CGRectContainsPoint([_catapultArm boundingBox], touchLocation))
