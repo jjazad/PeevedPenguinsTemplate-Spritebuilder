6872485600cb03c0838fa9260ae9a4339d427550
John <jzofrea@yahoo.com>
2014-07-15 10:14:16 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -69,13 +69,15 @@
 }
 
 
-/*
 - (void)touchMoved:(UITouch *)touch withEvent:(UIEvent *)event
 {
         // whenever touches move, update the position of the mouseJointNode to the touch position
     CGPoint touchLocation = [touch locationInNode:_contentNode];
     _mouseJointNode.position = touchLocation;
 }
+
+
+/*
 
 - (void)releaseCatapult {
     if (_mouseJoint != nil)
