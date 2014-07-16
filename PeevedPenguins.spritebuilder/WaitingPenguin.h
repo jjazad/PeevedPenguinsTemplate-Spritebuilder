4adbc2b41bccde3790a0859c8dddac7e7650f880
John <jzofrea@yahoo.com>
2014-07-16 11:45:03 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/WaitingPenguin.h
@@ -0,0 +1,13 @@
+//
+//  WaitingPenguin.h
+//  PeevedPenguins
+//
+//  Created by John Zofrea on 7/16/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface WaitingPenguin : CCSprite
+
+@end
---
+++ PeevedPenguins.spritebuilder/WaitingPenguin.m
@@ -0,0 +1,13 @@
+//
+//  WaitingPenguin.m
+//  PeevedPenguins
+//
+//  Created by John Zofrea on 7/16/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "WaitingPenguin.h"
+
+@implementation WaitingPenguin
+
+@end
